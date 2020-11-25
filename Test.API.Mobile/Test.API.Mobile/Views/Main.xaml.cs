38f0dd7a3282680a8f4911fd924eb14b77cf8450
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-11-25 22:50:22 +0330

    update mobile View

--- Test.API.Mobile/Test.API.Mobile/Views/Main.xaml.cs
+++ Test.API.Mobile/Test.API.Mobile/Views/Main.xaml.cs
@@ -3,19 +3,18 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Test.API.Mobile.ViewModels;
+
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace Test.API.Mobile.Views
 {
     [XamlCompilation(XamlCompilationOptions.Compile)]
-    public partial class LoginPage : ContentPage
+    public partial class Main : ContentPage
     {
-        public LoginPage()
+        public Main()
         {
             InitializeComponent();
-            this.BindingContext = new LoginViewModel();
         }
     }
 }
