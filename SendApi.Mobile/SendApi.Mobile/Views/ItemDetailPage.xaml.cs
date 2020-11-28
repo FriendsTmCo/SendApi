1fa79b4e55f615ae3710d76e7524db40b4be4c22
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-11-29 01:13:11 +0330

    insert Mobile Project

---
+++ SendApi.Mobile/SendApi.Mobile/Views/ItemDetailPage.xaml.cs
@@ -0,0 +1,15 @@
+﻿using SendApi.Mobile.ViewModels;
+using System.ComponentModel;
+using Xamarin.Forms;
+
+namespace SendApi.Mobile.Views
+{
+    public partial class ItemDetailPage : ContentPage
+    {
+        public ItemDetailPage()
+        {
+            InitializeComponent();
+            BindingContext = new ItemDetailViewModel();
+        }
+    }
+}
