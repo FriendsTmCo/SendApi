c6a0aac04a4bfb89ad8d1ba7765defa5b45108ba
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-04 20:29:37 +0330

    update view

--- Windows.App/Pages/HomeContent.xaml.cs
+++ Windows.App/Pages/HomeContent.xaml.cs
@@ -5,6 +5,10 @@ using System.Linq;
 using System.Runtime.InteropServices.WindowsRuntime;
 using Windows.Foundation;
 using Windows.Foundation.Collections;
+using Windows.System;
+using Windows.UI.Core;
+using Windows.UI.Popups;
+using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 using Windows.UI.Xaml.Controls.Primitives;
@@ -22,6 +26,8 @@ namespace Windows.App.Pages
     /// </summary>
     public sealed partial class HomeContent : Page
     {
+        public DateTime date = DateTime.Now;
+
         public HomeContent()
         {
             this.InitializeComponent();
@@ -33,5 +39,28 @@ namespace Windows.App.Pages
             var option = (MenuFlyoutItem)sender;
             drp_Type.Content = option.Tag;
         }
+
+        private void RadioButton_Checked(Object sender, RoutedEventArgs e)
+        {
+            var checkBox = (RadioButton)sender;
+            if (checkBox.Name == "raw")
+            {
+                if (checkBox.IsChecked.Value)
+                {
+                    drp_dataType.IsEnabled = true;
+                }
+                else
+                {
+                    drp_dataType.IsEnabled = false;
+                }
+            }
+        }
+
+        private void dataTypeChange(Object sender, RoutedEventArgs e)
+        {
+            var option = (MenuFlyoutItem)sender;
+            drp_dataType.Content = option.Tag;
+        }
+
     }
 }
