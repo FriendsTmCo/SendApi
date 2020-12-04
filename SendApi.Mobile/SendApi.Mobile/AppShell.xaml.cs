12ddcca9c4e0a2cd4f3a3658ec1aebee984a4752
Fri2ends <63165093+Fri2ends@users.noreply.github.com>
2020-12-04 22:39:05 +0330

    Update Mobile and Insert UWP

--- SendApi.Mobile/SendApi.Mobile/AppShell.xaml.cs
+++ SendApi.Mobile/SendApi.Mobile/AppShell.xaml.cs
@@ -11,8 +11,7 @@ namespace SendApi.Mobile
         public AppShell()
         {
             InitializeComponent();
-            Routing.RegisterRoute(nameof(ItemDetailPage), typeof(ItemDetailPage));
-            Routing.RegisterRoute(nameof(NewItemPage), typeof(NewItemPage));
+           
         }
 
         private async void OnMenuItemClicked(object sender, EventArgs e)
--- SendApi.Mobile/SendApi.Mobile/ViewModels/LoginViewModel.cs
+++ SendApi.Mobile/SendApi.Mobile/ViewModels/LoginViewModel.cs
@@ -1,24 +1,24 @@
-﻿using SendApi.Mobile.Views;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using Xamarin.Forms;
+﻿//using SendApi.Mobile.Views;
+//using System;
+//using System.Collections.Generic;
+//using System.Text;
+//using Xamarin.Forms;
 
-namespace SendApi.Mobile.ViewModels
-{
-    public class LoginViewModel : BaseViewModel
-    {
-        public Command LoginCommand { get; }
+//namespace SendApi.Mobile.ViewModels
+//{
+//    public class LoginViewModel : BaseViewModel
+//    {
+//        public Command LoginCommand { get; }
 
-        public LoginViewModel()
-        {
-            LoginCommand = new Command(OnLoginClicked);
-        }
+//        public LoginViewModel()
+//        {
+//            LoginCommand = new Command(OnLoginClicked);
+//        }
 
-        private async void OnLoginClicked(object obj)
-        {
-            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
-            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
-        }
-    }
-}
+//        private async void OnLoginClicked(object obj)
+//        {
+//            // Prefixing with `//` switches to a different navigation stack instead of pushing to the active one
+//            await Shell.Current.GoToAsync($"//{nameof(AboutPage)}");
+//        }
+//    }
+//}
